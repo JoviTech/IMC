d9cf9157b8c8a7ceb74e049acc57325db9dda250
Jovitech <jovitechgaropaba@gmail.com>
2023-06-15 19:44:49 -0300

    structuring the modal data

--- main.js
+++ main.js
@@ -3,16 +3,39 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+const Modal = {
+    
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
 
-form.onsubmit = handleSubmit
+    open: function() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
 
-// Funções
-
-function handleSubmit(evento){
-    evento.preventDefault()
+form.onsubmit = event => {
+    event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    console.log(weight, height)
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+    
+}
+
+Modal.buttonClose.onclick = () => {
+    Modal.close()
+}
+
+
+function IMC(weight, height){
+    return (weight / ((height / 100)**2)).toFixed(2)
 }
