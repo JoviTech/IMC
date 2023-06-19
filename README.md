257658d0d7fa551b9c1e7851bea6e86741513bc7
Jovitech <jovitechgaropaba@gmail.com>
2023-06-19 17:25:56 -0300

    add README

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <img src="./imagens/layout.png" alt="Demonstração do projeto" width="100%" />
+</p>
+
+## 🖥️ Projeto
+
+Esse é um projeto Web para calcular o IMC.
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido durante o curso Explorer da Rocketseat (stage 5) com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+- Git e Github
+
+## 🏷️ LAYOUT
+
+Você pode visualizar o layout do projeto através
+[desse link](<https://www.figma.com/file/IYTbZQLrh0JknmOIwKqaK6/IMC-(Copy)?type=design&node-id=108-2&t=TualEyfJbHik4a7K-0>).
+É necessário ter uma conta no [Figma](https://www.figma.com).
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alertError.js'
-import { IMC, notNumber } from './utils.js'
+import { calculateIMC, notNumber } from './utils.js'
 
 // variáveis - variables
 const form = document.querySelector('form')
@@ -15,18 +15,22 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const showAlertError = notNumber(weight) || notNumber(height)
-    if(showAlertError){
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+    if(weightOrHeightIsNotANumber){
         AlertError.open()
         return;
     }
     AlertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+    
+}
+
+function displayResultMessage(result){
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
     Modal.open()
-    
 }
 
